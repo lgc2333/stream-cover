565d2645eec9d6045c6df5f97d1a346d7fe5df81
student_2333 <lgc2333@126.com>
2024-11-26 18:30:45 +0800

    up

--- js/svg-autosize.js
+++ js/svg-autosize.js
@@ -3,7 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const elems = document.querySelectorAll('svg[data-autosize]')
   elems.forEach((svg) => {
     const { width, height } = svg.getBBox()
-    svg.setAttribute('width', width)
-    svg.setAttribute('height', height)
+
+    const additionalWAttr = svg.getAttribute('data-additional-w')
+    let additionalW = additionalWAttr ? parseFloat(additionalWAttr) : 0
+    if (Number.isNaN(additionalW)) additionalW = 0
+
+    const additionalHAttr = svg.getAttribute('data-additional-h')
+    let additionalH = additionalHAttr ? parseFloat(additionalHAttr) : 0
+    if (Number.isNaN(additionalH)) additionalH = 0
+
+    svg.setAttribute('width', width + additionalW)
+    svg.setAttribute('height', height + additionalH)
   })
 })
