0c0002068d87386b16c8e2918e37d2b9506a8c00
student_2333 <lgc2333@126.com>
2025-01-18 21:06:57 +0800

    up

--- src/js/time-now.js
+++ src/js/time-now.js
@@ -1,15 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
-  setInterval(() => {
+  const task = () => {
     /** @type {NodeListOf<HTMLSpanElement>} */
     const elems = document.querySelectorAll('span#time-now')
     const d = new Date()
-    const str =
-      `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}` +
-      ` ${d.getHours()}` +
-      `:${d.getMinutes().toString().padStart(2, '0')}` +
-      `:${d.getSeconds().toString().padStart(2, '0')}`
-    elems.forEach((span) => {
-      span.textContent = str
+
+    const year = `${d.getFullYear()}`
+    const yearShort = year.slice(-2)
+    const month = `${d.getMonth() + 1}`
+    const monthPad = month.padStart(2, '0')
+    const date = `${d.getDate()}`
+    const datePad = date.padStart(2, '0')
+    const hours = `${d.getHours()}`
+    const hoursPad = hours.padStart(2, '0')
+    const minutes = `${d.getMinutes()}`
+    const minutesPad = minutes.padStart(2, '0')
+    const seconds = `${d.getSeconds()}`
+    const secondsPad = seconds.padStart(2, '0')
+
+    elems.forEach((e) => {
+      e.textContent = (e.dataset.format || 'YYYY-mm-dd HH:MM:SS')
+        .replaceAll('YYYY', year)
+        .replaceAll('YY', yearShort)
+        .replaceAll('mm', monthPad)
+        .replaceAll('m', month)
+        .replaceAll('dd', datePad)
+        .replaceAll('d', date)
+        .replaceAll('HH', hoursPad)
+        .replaceAll('H', hours)
+        .replaceAll('MM', minutesPad)
+        .replaceAll('M', minutes)
+        .replaceAll('SS', secondsPad)
+        .replaceAll('S', seconds)
     })
-  }, 500)
+  }
+
+  setInterval(task, 500)
+  task()
 })
