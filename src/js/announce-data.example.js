55e100c631b62952e56ae9dd22b94048dfc55ca7
LgCookie <lgc2333@126.com>
2025-02-09 20:43:33 +0800

    up

--- src/js/announce-data.example.js
+++ src/js/announce-data.example.js
@@ -1,11 +1,13 @@
-globalThis.announceData = {
-  example: [
-    [
-      'Create src/js/announce.js to use your custom messages',
-      'Toggle different announce groups using OBS browser source additional css',
-      '[id^="announce-"] { display: none }',
-      '#announce-example-2 { display: inherit }',
+;(() => {
+  globalThis.announceData = {
+    example: [
+      [
+        'Create src/js/announce.js to use your custom messages',
+        'Toggle different announce groups using OBS browser source additional css',
+        '[id^="announce-"] { display: none }',
+        '#announce-example-2 { display: inherit }',
+      ],
     ],
-  ],
-  'example-2': [['If you moved this to the left', 'You got it!']],
-}
+    'example-2': [['If you made the text on the left disappear,', 'You got it!']],
+  }
+})()
