fb70672158cdbc7bdf05fd88b0c7c1202adb6da4
LgCookie <lgc2333@126.com>
2025-03-24 20:50:32 +0800

    up

--- src/js/announce.js
+++ src/js/announce.js
@@ -1,16 +1,22 @@
 document.addEventListener('DOMContentLoaded', async () => {
-  if (!globalThis.announceData) return
-  const containerEl = document.getElementById('announce')
-  Object.entries(globalThis.announceData).forEach(([key, value]) => {
-    const groupContainerEl = document.createElement('div')
-    groupContainerEl.id = `announce-${key}`
+  if (globalThis.announceData) {
+    const containerEl = document.getElementById('announce')
+    Object.entries(globalThis.announceData).forEach(([key, value]) => {
+      const groupContainerEl = document.createElement('div')
+      groupContainerEl.id = `announce-${key}`
 
-    value.forEach((x) => {
-      const el = document.createElement('div')
-      el.innerText = x.join('\n')
-      groupContainerEl.appendChild(el)
+      value.forEach((x) => {
+        const el = document.createElement('div')
+        el.innerText = x.join('\n')
+        groupContainerEl.appendChild(el)
+      })
+
+      containerEl.appendChild(groupContainerEl)
     })
+  }
 
-    containerEl.appendChild(groupContainerEl)
-  })
+  if (globalThis.copyrightAnnounceData) {
+    const el = document.getElementById('right-announce-copyright')
+    el.innerText = globalThis.copyrightAnnounceData.join('\n')
+  }
 })
