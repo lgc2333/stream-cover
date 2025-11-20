fef3e97b1586dd742394df95b67b9ca98edac6d6
LgCookie <lgc2333@126.com>
2025-11-20 20:04:48 +0800

    up

--- src/js/announce-data.example.js
+++ src/js/announce-data.example.js
@@ -1,19 +1,21 @@
 ;(() => {
   globalThis.announceData = {
-    example: [
-      [
-        'Create src/js/announce.js to use your custom messages',
-        'Toggle different announce groups using OBS browser source additional css',
-        '[id^="announce-"] { display: none }',
-        '#announce-example-2 { display: inherit }',
-      ],
-    ],
-    'example-2': [['If you made the text on the left disappear,', 'You got it!']],
+    // I recommend put announces via OBS text element now
+    // If you still want to use this it's OK
+    // example: [
+    //   [
+    //     'Create src/js/announce.js to use your custom messages',
+    //     'Toggle different announce groups using OBS browser source additional css',
+    //     '[id^="announce-"] { display: none }',
+    //     '#announce-example-2 { display: inherit }',
+    //   ],
+    // ],
+    // 'example-2': [['If you made the text on the left disappear,', 'You got it!']],
   }
   globalThis.copyrightAnnounceData = [
-    'Your name',
-    'https://live.bilibili.com/8333250',
+    'LgCookie & Misght',
+    'Bilibili Live 8333250 & 8044503',
+    '背景设计 @浅明忆想 | 装修开源地址↓',
     'github.com/lgc2333/stream-cover',
-    '背景设计 @浅明忆想',
   ]
 })()
